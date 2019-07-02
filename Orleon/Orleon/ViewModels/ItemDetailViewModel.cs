0928659a98348c96863a87bf0216ffdf8da4cb16
Stephen Schaal <stephen.schaal@gmail.com>
2019-07-02 15:04:09 -0400

    init

---
+++ Orleon/Orleon/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Orleon.Models;
+
+namespace Orleon.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
